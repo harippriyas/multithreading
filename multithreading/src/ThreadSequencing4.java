c891fcf20be7e624693fa23f23bc5e0f56084789
Harippriya Sivapatham <Haripriya@microfocus.com>
2023-05-18 18:06:48 +0530

    GoldmanSachs problem

--- multithreading/src/ThreadSequencing4.java
+++ multithreading/src/ThreadSequencing4.java
@@ -1,98 +1,90 @@
+/**
+ * Goldman-sachs: Print series 010203040506.
+ * Using multi-threading 1st thread will print only 0.
+ * 2nd thread will print only even numbers and 3rd thread print only odd numbers.
+ */
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/** Scenario: Print series 010203040506. Using multi-threading 1st thread will print only 0 2nd thread will print only even numbers and 3rd thread print only odd numbers.
- *  
- *  */
-public class ThreadSequencing4
-{
-	public static enum Type { ZERO, ODD, EVEN };
-	
-	private Object lock = new Object();
-	private AtomicInteger numToPrint = new AtomicInteger(1);
-	private volatile int prevThread = Type.ZERO.ordinal();
-	private volatile int nextThread = Type.ZERO.ordinal();
+public class ThreadSequencing4 implements Runnable{
+    public static int NUM_ITERATIONS = 10;
 
-	public MyThread createThreadObj(int threadId)
-	{
-		return new MyThread(threadId);
-	}
-	
-	public class MyThread implements Runnable
-	{
-		private int threadId;
+    public static AtomicInteger counter = new AtomicInteger(1);
 
-		public MyThread(int threadId)
-		{
-			this.threadId = threadId;
-		}
-		
-		@Override
-		public void run() 
-		{
-			try
-			{
-				while(numToPrint.get() <= 10)
-				{
-					synchronized(lock)
-					{
-						if(nextThread == threadId)
-						{
-							if(threadId == Type.ZERO.ordinal())
-							{
-								System.out.print("0");
-								if(prevThread == Type.ODD.ordinal())
-								{
-									nextThread = Type.EVEN.ordinal();
-								}
-								else
-								{
-									nextThread = Type.ODD.ordinal();
-								}
-							}
-							else if(threadId == Type.ODD.ordinal())
-							{
-								System.out.print(numToPrint.getAndIncrement());
-								prevThread = Type.ODD.ordinal();
-								nextThread = Type.ZERO.ordinal();
-							}
-							else if(threadId == Type.EVEN.ordinal())
-							{
-								System.out.print(numToPrint.getAndIncrement());
-								prevThread = Type.EVEN.ordinal();
-								nextThread = Type.ZERO.ordinal();
-							}
-							
-							lock.notifyAll();
-						}
-						else
-						{
-							// this is not enough to keep the thread alive. 
-							// You need the while at the top so that the run() does not exit until we print the resource for this thread.
-							lock.wait();
-						}
-					}
-				}
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-			
-		}
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		ThreadSequencing4 ts = new ThreadSequencing4();
-		
-		Thread t1 = new Thread(ts.createThreadObj(ThreadSequencing4.Type.ZERO.ordinal()));
-		Thread t2 = new Thread(ts.createThreadObj(ThreadSequencing4.Type.ODD.ordinal()));
-		Thread t3 = new Thread(ts.createThreadObj(ThreadSequencing4.Type.EVEN.ordinal()));
-		
-		t1.start();
-		t2.start();
-		t3.start();
-		
-	}
+    public static Semaphore zeroSem = new Semaphore(1);
+    public static Semaphore oddSem = new Semaphore(1);
+    public static Semaphore evenSem = new Semaphore(1);
+    private int threadId;
+    public ThreadSequencing4(int num){
+        this.threadId = num;
+    }
+
+    public void run() {
+        for(int i=0; i<NUM_ITERATIONS; i++) {
+            try {
+                if(threadId == 0) {
+                    zeroSem.acquire();
+                    System.out.print("0");
+                    if(counter.get() % 2 == 0) {
+                        evenSem.release();
+                    }
+                    else {
+                        oddSem.release();
+                    }
+
+                }
+                else if(threadId == 1) {
+                    oddSem.acquire();
+                    System.out.print(counter.getAndIncrement());
+                    zeroSem.release();
+                }
+                else {
+                    evenSem.acquire();
+                    System.out.print(counter.getAndIncrement());
+                    zeroSem.release();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public static void main(String[] args){
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ThreadSequencing4 t1 = new ThreadSequencing4(0);
+        ThreadSequencing4 t2 = new ThreadSequencing4(1);
+        ThreadSequencing4 t3 = new ThreadSequencing4(2);
+
+        try {
+            zeroSem.acquire();
+            oddSem.acquire();
+            evenSem.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        executorService.submit(t1);
+        executorService.submit(t2);
+        executorService.submit(t3);
+
+        zeroSem.release();
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+            System.out.println();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            System.out.println("Shutting down executor");
+            executorService.shutdownNow();
+        }
+
+    }
+
+
+
 }
